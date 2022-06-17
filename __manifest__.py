37aa3d671f46f7860e9ade969492638ab4f73dbc
Mario Roberto <84170744+mrgomez84@users.noreply.github.com>
2022-06-17 14:45:10 -0600

    required= ok
    Campos obligatorio = OK
    Requerimientos de Prueba Tecnica Completados

--- __manifest__.py
+++ __manifest__.py
@@ -24,12 +24,12 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
--- models/models.py
+++ models/models.py
@@ -7,10 +7,10 @@ class treming_mario_gomez(models.Model):
     _name = 'treming_mario_gomez.treming_mario_gomez'
     _description = 'treming_mario_gomez.treming_mario_gomez'
 
-    date = fields.Datetime(string='Fecha',use='required')
-    name = fields.Char(string='Nombre del Visitante',use='required')
-    value = fields.Many2one('res.partner', string='Paciente',use='required')
-    description = fields.Text(use='required')
+    date = fields.Datetime(string='Fecha')
+    name = fields.Char(string='Nombre del Visitante')
+    value = fields.Many2one('res.partner', string='Paciente')
+    description = fields.Text()
 
     # @api.depends('value')
     # def _value_pc(self):
